376383a8649d01f3cd5b39feeede08f1b4ba669c
Michelle <earlycapistran@gmail.com>
2024-10-15 16:04:37 -0700

    Initial commit

---
+++ R/03_green_data_wrangling.R
@@ -0,0 +1,13 @@
+# Green turtle data wrangling
+
+# Load data and libraries
+library(dplyr)
+full_data <- readRDS("./data/processed/full_processed_data_historical.rds")
+
+# Select primary columns of interest
+green_data <- full_data %>% 
+  select(-c(p_number, secondary_p_number, url, num_turtles, length, weight))
+
+# Select green turtles
+green_data <-green_data %>% 
+  filter(species == "green")
