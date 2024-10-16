9a3a4b3937a44cfd0d97ce874012d9632146dab0
Michelle <earlycapistran@gmail.com>
2024-10-16 15:18:58 -0700

    Initial data quality indices

--- R/04_data_quality_index.R
+++ R/04_data_quality_index.R
@@ -17,4 +17,29 @@ historical <- historical %>%
     location_certainty == "not_reported" ~ 0))
 
 historical <- historical %>% 
-  mutate(spp_id_dummy = ifelse(spp_id == "reported", 1, 0))
+  mutate(spp_id_dummy = ifelse(spp_id == "reported", 1, 0)) %>% 
+  mutate(weight_dummy = ifelse(is.na(weight_kg), 0, 1)) %>% 
+  mutate(length_dummy = ifelse(is.na(length_cm), 0, 1)) %>% 
+  mutate(habitat_dummy = ifelse(habitat == "not_reported", 0, 1)) %>% 
+  mutate(narrative_detail = as.numeric(narrative_detail))
+
+# Calculate data quality index as sum of location certainty, 
+# spp_id, narrative detail and presence of weight, length, 
+# and/or habitat
+
+historical <- historical %>% 
+  mutate(data_quality_index = rowSums(
+    across(c(location_certainty_dummy,
+             spp_id_dummy,
+             weight_dummy,
+             length_dummy,
+             habitat_dummy,
+             narrative_detail))))
+
+# Save another data quality index as ordinal
+historical <- historical %>%
+  mutate(data_quality_cat = case_when(
+    between(data_quality_index, 1,3) ~ "low",
+    between(data_quality_index, 4,6) ~ "medium",
+    between(data_quality_index, 7, 9) ~ "high"))
+
