c9f8ebce0d45fc1faea560af10032359d1504c38
Michelle <earlycapistran@gmail.com>
2024-10-16 11:50:10 -0700

    Wrangled rows with multiple locations so each one has a unique row.

--- R/03_data_cleaning_locations.R
+++ R/03_data_cleaning_locations.R
@@ -1,6 +1,8 @@
-# Load data
+# Load data and libraries
 location <- read.csv("./data/raw/location_raw.csv")
 historical <- readRDS("./data/processed/full_processed_data_historical.rds")
+library("dplyr")
+library("string")
 
 # Compare site names across datasets ----------------------------
 head(location)
@@ -25,6 +27,20 @@ locations_processed <- location %>%
 # Compare again with reverse order and save output
 setdiff(locations_historical, locations_processed$location)
 mult_loc <- unlist(setdiff(locations_historical, locations_processed$location))
+mult_loc <- mult_loc[ !mult_loc == 'not_reported'] # We want to keep these in the original df
 
-# Let's duplicate these rows in the historical dataset
-loc_historical_duplicate <- historical %>% filter(location %in% mult_loc)
+# Make a new dataframe to parse out rows with multiple locations
+historical_2 <- historical %>% 
+  filter(location %in% mult_loc) %>% 
+  # Split sites into two new columns (one for each)
+  separate(location, into = c('loc_1', 'loc_2'), ",", convert = TRUE) %>% 
+  # Pivot long to make one row for each location
+  pivot_longer(cols=c('loc_1', 'loc_2'),
+               values_to='location') %>% 
+  select(-c("name")) %>% 
+  mutate(location = str_trim(location)) # Remove whitespace
+
+# Put everything together and remove rows with multiple sites
+historical_3 <- rbind(historical_2, historical) %>% 
+  filter(!location %in% mult_loc)
+
