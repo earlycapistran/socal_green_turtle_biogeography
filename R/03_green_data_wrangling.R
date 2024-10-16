6d38376a2954f47aca19d4d9f251fb2e9395dd22
Michelle <earlycapistran@gmail.com>
2024-10-16 10:45:39 -0700

    Fixed syntax

--- R/03_green_data_wrangling.R
+++ R/03_green_data_wrangling.R
@@ -6,7 +6,12 @@ full_data <- readRDS("./data/processed/full_processed_data_historical.rds")
 
 # Select primary columns of interest
 green_data <- full_data %>% 
-  select(-c(p_number, secondary_p_number, url, num_turtles, length, weight))
+  select(-c(p_number, 
+            secondary_p_number, 
+            url, 
+            num_turtles, 
+            length_ft_inches, 
+            weight_lb))
 
 # Select green turtles
 green_data <-green_data %>% 
