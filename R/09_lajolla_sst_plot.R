9fae3c2b3a9bb066689e6d2a3f9757e954a651b4
Michelle <earlycapistran@gmail.com>
2025-11-18 14:55:56 -0800

    Added as bar chart

--- R/09_lajolla_sst_plot.R
+++ R/09_lajolla_sst_plot.R
@@ -2,6 +2,7 @@
 library("tidyverse")
 library("janitor")
 library("zyp")
+library("RColorBrewer")
 
 # Load data
 base_data <- read.csv("./data/raw/scripps_lajolla_temp/LaJolla_temp_1916-202506_dataOnly.csv")
@@ -10,6 +11,9 @@ base_data <- read.csv("./data/raw/scripps_lajolla_temp/LaJolla_temp_1916-202506_
 # devtools::install_github("thomasp85/scico") Scientific color maps
 library("scico")
 theme_set(theme_light())
+
+# Alternate: RColorBrewer
+col_strip <- brewer.pal(11, "RdBu")
 
 # Check data structure
 str(base_data)
@@ -37,7 +41,7 @@ graphing_temps <- filtered_data %>%
 
 # Calculate anomaly and add to dataframe
 graphing_temps <- graphing_temps %>% 
-  mutate(temp_anomaly = annual_mean_sst - baseline_temp)
+  mutate(temp_anomaly = annual_mean_sst - baseline_sst)
 
 # Look over data ---------------------------------------------------
 graphing_temps %>% skimr::skim()
@@ -64,26 +68,35 @@ graphing_temps %>%
     y = NULL
   )
 
-# Statistical tests -------------------------------------------
-
-# Linear regression
+# Plot with RColorBrewer----------------------------------------------
 graphing_temps %>% 
-  ggplot(aes(x = year, y = annual_mean_sst)) +
-  geom_point() +
-  geom_smooth(method = "lm")
+  ggplot(aes(x = year, y = 1, fill = temp_anomaly)) +
+  geom_tile() +
+  scale_fill_gradientn(colors = rev(col_strip))  +
+  theme(
+    legend.position = "none",
+    panel.grid = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank()
+  ) +
+  labs(
+    x = "Year",
+    y = NULL,
+    title = "La Jolla Pier, 1916-2025",
+  )
 
-model <- lm(year ~ annual_mean_sst, data = graphing_temps)
-summary(model)
-summary(model)$r.squared
 
-# Mann-Kendall
-mk_result <- MannKendall(graphing_temps$annual_mean_sst)
-mk_result
-
-# Calculate Kendall's tau and its associated p-value
-tau_result <- cor.test(graphing_temps$annual_mean_sst,
-                       graphing_temps$year, 
-                       method = "kendall")
-
-# Print Kendall's tau and its associated p-value
-print(tau_result)
+graphing_temps %>% 
+  ggplot(aes(x = year, y = temp_anomaly, fill = temp_anomaly)) +
+  geom_col() +
+  scale_fill_gradientn(colors = rev(col_strip))  +
+  geom_hline(yintercept = 0, lty = 2) +
+  theme(legend.position = "none") +
+  guides(fill = guide_colorbar(barwidth = 25, barheight = 1, title.position = "top", title.hjust = 0.5)) +
+  labs(
+    x = "Year",
+    y = NULL,
+    title = "Temperature Change at La Jolla Pier",
+    subtitle = "Relative to average of 1916-2025 [°C]"
+  )
+  
