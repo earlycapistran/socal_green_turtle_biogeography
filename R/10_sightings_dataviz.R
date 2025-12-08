d1c6c9869718b9a1e4047f214ae37d0cbc63ab4d
Michelle <earlycapistran@gmail.com>
2025-12-08 15:21:45 -0800

    Nice working plot

--- R/10_sightings_dataviz.R
+++ R/10_sightings_dataviz.R
@@ -1,58 +1,40 @@
 data <- readRDS("./data/processed/full_data_green.rds")
 library(tidyverse)
 
-# Select variables of interest
 plot_data <- data %>% 
   select(num_turtles_numeric, lat_group) %>% 
-  # Remove datapoints with unreported locations
   na.omit()
 
-# Plot
-ggplot(plot_data, aes(x = lat_group, 
-                      y = num_turtles_numeric, 
-                      color = lat_group)) +
-  # 1. Boxplot (behind points)
-  geom_boxplot(
-    width = 0.25, 
-    alpha = 0.5, 
-    outlier.shape = 4,
-    outlier.size = 4,  # Slightly smaller for balance
-    color = "grey20",
-    show.legend = FALSE  # Hide duplicate legend
-  ) +
-  
-  # 2. Jittered points (on top)
-  geom_jitter(
-    size = 3,
-    width = 0.05,
-    alpha = 0.6,
-    stroke = 0.5  # For better point definition
-  ) +
-  
-  # Color scheme
-  scale_color_manual(
-    name = NULL,
-    values = c("#a86826", "#3d6721"),
-    labels = c("Southern California Bight", 
-               "South of Southern California Bight")
-  ) +
-  
-  # Labels
-  labs(
-    x = NULL,  # No x-axis label
-    y = "Number of Turtles per Report (by location)"
-  ) +
-
-  
-  # Theme adjustments
-  theme_classic(base_size = 18) +
+ggplot(plot_data, aes(x = num_turtles_numeric, y = lat_group, fill = lat_group)) +
+  geom_dotplot(binaxis = "x",           # CHANGED from "y"
+               stackdir = "up",
+               dotsize = 2.25,
+               binwidth = 1,
+               stackratio = 0.75,
+               color = NA,
+               alpha = 0.9) +
+  geom_boxplot(width = 0.15,             # Now controls height
+               alpha = 0.3, 
+               outlier.shape = NA,
+               color = "grey20",
+               position = position_nudge(y = -0.2)) +
+  # Color scheme - use colorblind-friendly palette
+  scale_fill_manual(values = c("north" = "#f0a226", "south" = "#28a685")) +
+  scale_y_discrete(labels = c("north" = "Southern California\nBight", 
+                              "south" = "South of Southern\nCalifornia Bight")
+                   ) + 
+  facet_grid(lat_group ~ ., 
+             scales = "free_y",
+             space = "free_y") +
+  labs(y = "Region", x = "Number of Turtles per Report (by location)") +
+  coord_cartesian(xlim = c(0, NA)) +
+  theme_classic(base_size = 14) +
   theme(
-    text = element_text(family = "Lato"),  # Apply Lato to all text
-    axis.text.x = element_blank(),  # Remove x-axis text completely
-    axis.ticks.x = element_blank(),  # Remove x-axis ticks
-    legend.position = "bottom",
-    legend.text = element_text(size = 10),
-    panel.grid.major.x = element_blank(),  # Remove vertical grid lines
-    plot.title = element_text(face = "bold"),
-    plot.margin = unit(c(5,5,5,5), "pt")
-  ) 
+    # Font
+    element_text(family = "Lato"),
+    legend.position = "none",
+    axis.title = element_text(face = "bold"),
+    panel.grid.major.x = element_line(color = "gray90", size = 0.3),
+    strip.background = element_blank(),  # removes facet background
+    strip.text = element_blank()         # removes facet text
+  )
